d84d666bf434c7de56c337be63f46dc654fb6363
Benjamin Giesinger <benjamin.giesinger@gmail.com>
2019-10-11 10:16:00 +0200

    Fixed build errors

--- app/src/main/java/com/newbucket/ukulelechords/UkeFretView.java
+++ app/src/main/java/com/newbucket/ukulelechords/UkeFretView.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
